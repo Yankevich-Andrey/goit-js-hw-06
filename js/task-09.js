e67b416a33ff09b75c5be6e1f287d304ce8fa894
Yankevich-Andrey <yankevichmail@gmail.com>
2023-09-04 19:39:04 +0300

    09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+});
